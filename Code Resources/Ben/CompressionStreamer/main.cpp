8a16bb834a5653dd3c9518b82d83d194aaa272e6
bennahugo@gmail.com <bennahugo@gmail.com@581c70f1-3466-16af-cd1f-e6c50acc8059>
2013-09-25 09:01:08 +0000

    updated cuda version to copy a timestamp worth of prefix and residual data at a time

--- Code Resources/Ben/CompressionStreamer/main.cpp
+++ Code Resources/Ben/CompressionStreamer/main.cpp
@@ -35,6 +35,65 @@ double totalDecompressWriteTime = 0;
 double totalDiskReadTime = 0;
 int main(int argc, char **argv) {
     using namespace std;
+    
+//     gpuCode::initCUDA();
+//     float* iv = new float[15];
+//     float* n = new float[15];
+//    
+//     iv[0] = 0.4235811f;
+//     iv[1] = 3.5687154f;
+//     iv[2] =-1.2547897f;
+//     iv[3] = 3.5687154f;
+//     iv[4] =-1.2547897f;
+//     iv[5] = 3.5687154f;
+//     iv[6] =-1.2547897f;
+//     iv[7] = 3.5687154f;
+//     iv[8] =-1.2547897f;
+//     iv[9] = 3.5687154f;
+//     iv[10] =-1.2547897f;
+//     iv[11] = 3.5687154f;
+//     iv[12] =-1.2547897f;
+//     iv[13] = 3.5687154f;
+//     iv[14] =-1.2547897f;
+//    
+//     n[0] = 0.4335811f;
+//     n[1] = 3.5587154f;
+//     n[2] =-1.2447897f;
+//     n[3] = 3.5587154f;
+//     n[4] =-1.2447897f;
+//     n[5] = 3.5587154f;
+//     n[6] =-1.2447897f;
+//     n[7] = 3.5587154f;
+//     n[8] =-1.2447897f;
+//     n[9] = 3.5587154f;
+//     n[10] =-1.2447897f;
+//     n[11] = 3.5587154f;
+//     n[12] =-1.2447897f;
+//     n[13] = 3.5587154f;
+//     n[14] =-1.2447897f;
+//    
+//     for (int i = 0; i < 15; ++i){
+//       uint32_t ivElm = *((uint32_t*)&iv[0] + i);
+//       uint32_t nElm = *((uint32_t*)&n[0] + i);
+//       uint32_t x = ivElm ^ nElm;
+//       printf("%d:::",i);
+//       printBinaryRepresentation((void*)&ivElm,sizeof(uint32_t));
+//       printf("(%d)",ivElm);
+//       cout << " ^ ";
+//       printBinaryRepresentation((void*)&nElm,sizeof(uint32_t));
+//       printf("(%d)",nElm);
+//       cout << " = ";
+//       printBinaryRepresentation((void*)&x,sizeof(uint32_t));
+//       cout << '\n';
+//     }
+//     gpuCode::compressor::initCompressor(iv,15);
+//     gpuCode::decompressor::initDecompressor(iv,15);
+//     gpuCode::compressor::compressData(n,15,compressCallback);
+//     gpuCode::releaseCard();
+//     delete[] iv;
+//     delete[] n;
+
+    
     if (argc < 2){
       cout << "FATAL: PLEASE SPECIFY MEERKAT HDF5 FILE LOCATION" << endl;
       exit(1);
@@ -150,6 +209,11 @@ void decompressCallback(uint32_t elementCount, uint32_t * decompressedData){
       }
     }
   }
+//   cout << "DECOMPRESSED DATA: " << endl;
+//   for (uint32_t i = 0; i < elementCount; ++i){
+//     printBinaryRepresentation((void*)&decompressedData[i],sizeof(uint32_t));
+//     std::cout << std::endl;
+//   }
   timer::tic();
   if (writeStream){
       fwrite(decompressedData,sizeof(uint32_t),elementCount,fdecomp);
@@ -159,26 +223,27 @@ void decompressCallback(uint32_t elementCount, uint32_t * decompressedData){
 void compressCallback(uint32_t elementCount, uint32_t * compressedResidualsIntCounts, uint32_t ** compressedResiduals,
 			    uint32_t * compressedPrefixIntCounts, uint32_t ** compressedPrefixes, 
 			    uint32_t chunkCount, uint32_t * chunkSizes){
+//     std::cout << "PREFIX ARRAY:" << std::endl;
+//     for (int i = 0; i < chunkCount; ++i){
+//       std::cout << ">>" << i << std::endl;
+//       for (int eI = 0; eI < compressedPrefixIntCounts[i]; ++eI){
+// 	printBinaryRepresentation((void*)&compressedPrefixes[i][eI],sizeof(uint32_t));
+// 	std::cout << std::endl;
+//       }
+//     }
+//     std::cout << "RESIDUAL ARRAY:" << std::endl;
+//     for (int i = 0; i < chunkCount; ++i){
+//       std::cout << ">>" << i << std::endl;
+//       for (int eI = 0; eI < compressedResidualsIntCounts[i]; ++eI){
+// 	printBinaryRepresentation((void*)&compressedResiduals[i][eI],sizeof(uint32_t));
+// 	std::cout << std::endl;
+//       }
+//     }
+    
     if (!skipDecompression){ 
       gpuCode::decompressor::decompressData(elementCount,chunkCount,chunkSizes,
  					 compressedResiduals,compressedPrefixes,decompressCallback);
     }
-    /*std::cout << "PREFIX ARRAY:" << std::endl;
-    for (int i = 0; i < chunkCount; ++i){
-      std::cout << ">>" << i << std::endl;
-      for (int eI = 0; eI < compressedPrefixIntCounts[i]; ++eI){
-	printBinaryRepresentation((void*)&compressedPrefixes[i][eI],sizeof(uint32_t));
-	std::cout << std::endl;
-      }
-    }
-    std::cout << "RESIDUAL ARRAY:" << std::endl;
-    for (int i = 0; i < chunkCount; ++i){
-      std::cout << ">>" << i << std::endl;
-      for (int eI = 0; eI < compressedResidualsIntCounts[i]; ++eI){
-	printBinaryRepresentation((void*)&compressedResiduals[i][eI],sizeof(uint32_t));
-	std::cout << std::endl;
-      }
-    }*/
     timer::tic();
     if (writeStream){
       for (uint32_t i = 0; i < chunkCount; ++i){
